e2703f3b84b3052017aa804fd2e1b2917476eb65
이정훈(XW - LEE JUNGHOON) <leejunghoon@koreanair.com>
2021-06-30 16:11:39 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -2,17 +2,43 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const {User} = require('./models/User')
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+//application/x-wwww-form-urlencoded 형태를 가져올수있겠끔
+app.use(bodyParser.urlencoded({extended:true}))
+
+//application/json
+app.use(bodyParser.json())
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://heroup:1234@boilerplate.u25sg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI, {
     useNewUrlParser:true, useUnifiedTopology:true, useFindAndModify:false, useCreateIndex:true
 }).then(()=>console.log('MongoDB Connected'))
     .catch(err => console.log(err))
 
-
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!')
+})
+
+
+app.post('/register', (req,res) =>{
+  //회원 가입 할때 필요한 정보들을 Client에서 가져오면
+  //그것을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if(err) return res.json({success:false, err})
+    return res.status(200).json({
+      success:true
+    })
+  })
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+  
